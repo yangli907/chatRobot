117693353e0496de7edbafa45f400d6273ae63ca
yangli907 <yangli907@ubuntu.(none)>
2013-04-22 00:19:44 -0700

    added lang menu

--- NewAndroid/src/com/yangli907/newandroid/MainActivity.java
+++ NewAndroid/src/com/yangli907/newandroid/MainActivity.java
@@ -19,13 +19,16 @@ import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.AssetManager;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -142,10 +145,13 @@ public class MainActivity extends Activity {
 	
 	private String sendRequest(String inputText){
 		List<String> cookies = getConnCookie(connUrl);
+		SharedPreferences prefs = PreferenceManager
+			    .getDefaultSharedPreferences(MainActivity.this);
+		String locale = prefs.getString("listpref", "en");
 		String msg = "";
 		Log.i("Language", langOpt.getCheckedRadioButtonId()==R.id.eng?"ENGLISH":"CHINESE");
 		//String locale = "zh";
-		String locale = langOpt.getCheckedRadioButtonId()==R.id.eng?"en":"zh";
+		//String locale = langOpt.getCheckedRadioButtonId()==R.id.eng?"en":"zh";
 		try {
 			Log.i("*****inputText*****", inputText);
 			msg = URLEncoder.encode(inputText,"UTF-8");
@@ -264,4 +270,16 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		    case R.id.action_settings:
+		    startActivity(new Intent(this, PrefsActivity.class));
+		    return true;
+		    default:
+		    return super.onOptionsItemSelected(item);
+		}
+	}
+	
+	
 }
